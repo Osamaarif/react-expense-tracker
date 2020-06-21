9eb26d47314be36de8a55214bd3698cb5205ef8b
Osamaarif <usamaarif74@yahoo.com>
2020-06-21 09:37:03 +0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Header } from './header';
+import { Balance } from './balance';
+import { AccountSummary } from './accountSummary';
+import { TransactionHistory } from './transactionHistory';
+import { AddTransaction } from './addTransaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <AccountSummary/>
+        <TransactionHistory/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
---
+++ src/addTransaction.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+
+export const AddTransaction = () => {
+
+    const [description, setDescription]=useState('');
+    const [transactionamount, setTransactionAmount]=useState(0);
+
+    return (
+        <div>
+            <h3>Add New Transaction</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="description">
+                        Description
+                    </label>
+                    <input  type="text"
+                            id="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder="Detail of Transaction" />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="transactionamount">
+                        Transaction Amount
+                    </label>
+                    <input  type="number"
+                            id="transactionamount"
+                            value={transactionamount}
+                            onChange={(e) => setTransactionAmount(e.target.value)}
+                            placeholder="Dollar value of Transaction"/>
+                </div>
+                <button className="btn">Add Transaction</button>
+            </form>
+        </div>
+    )
+}
